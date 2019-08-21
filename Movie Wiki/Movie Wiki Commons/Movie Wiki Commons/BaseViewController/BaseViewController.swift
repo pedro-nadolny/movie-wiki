d40d1d80dbef473268ed00d7765dc6d9449cd1bf
Pedro Nadolny <pedro.nadolny@gmail.com>
2019-08-21 01:54:30 -0300

    BaseViewController default initializer and Alert help methods

--- Movie Wiki/Movie Wiki Commons/Movie Wiki Commons/BaseViewController/BaseViewController.swift
+++ Movie Wiki/Movie Wiki Commons/Movie Wiki Commons/BaseViewController/BaseViewController.swift
@@ -1,8 +1,25 @@
 import UIKit
 
-class BaseViewController: UIViewController {
+open class BaseViewController: UIViewController {
+    
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
     func showAlert(with title: String?, and message: String?) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+        showAlertController(with: title, message: message, and: .alert)
+    }
+    
+    func showActionSheet(with title: String?, and message: String?) {
+        showAlertController(with: title, message: message, and: .actionSheet)
+    }
+    
+    fileprivate func showAlertController(with title: String?, message: String?, and type: UIAlertController.Style) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: type)
         
         alert.addAction (
             UIAlertAction(title: "Close", style: .default) { (action) in
