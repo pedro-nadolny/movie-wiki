064244a1dcc3101a099b0c91a952608dc2a8b401
Pedro Nadolny <pedro.nadolny@gmail.com>
2019-08-21 01:53:26 -0300

    Enhancements on coordinator protocol from Commons

--- Movie Wiki/Movie Wiki Commons/Movie Wiki Commons/Coordinator/Coordinator.swift
+++ Movie Wiki/Movie Wiki Commons/Movie Wiki Commons/Coordinator/Coordinator.swift
@@ -1,5 +1,18 @@
 import Foundation
+import Swinject
 
-protocol Coordinator {
+public protocol Coordinator: class {
+    var childCoordinators: [Coordinator] { get set }
+    var container: Container { get set }
     func start()
+}
+
+extension Coordinator {
+    func addChildCoordinator(_ coordinator: Coordinator) {
+        childCoordinators.append(coordinator)
+    }
+    
+    func removeChildCoordinator(_ coordinator: Coordinator) {
+        childCoordinators.removeAll { $0 === coordinator }
+    }
 }
